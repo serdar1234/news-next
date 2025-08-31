6abb22080af42d5788171115e4c71a43e137bdad
Serdar Geldyev <serdarabad@gmail.com>
2025-08-31 19:48:52 +0300

    fetching without useEffect

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,47 +1,18 @@
-"use client";
-
-// import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 import { News } from "@/lib/types";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isError, setError] = useState<boolean>(false);
-  const [news, setNews] = useState<News[]>([]);
-
-  useEffect(() => {
-    async function getNews() {
-      const result = await fetch("http://localhost:8080/news");
-      if (!result.ok) {
-        setIsLoading(false);
-        setError(true);
-      }
-
-      setIsLoading(false);
-      const data = await result.json();
-      setNews(data);
-    }
-
-    getNews();
-  }, []);
-
-  if (isLoading) {
-    return <h1>Loading...</h1>;
+export default async function NewsPage() {
+  const result = await fetch("http://localhost:8080/news");
+  if (!result.ok) {
+    throw new Error("Failed to fetch the data");
   }
 
-  if (isError) {
-    return <h1>Error occured while fetching the news</h1>;
-  }
+  const news: News[] = await result.json();
 
-  let newsContent = null;
-  if (news.length > 0) {
-    newsContent = <NewsList news={news} />;
-  }
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
